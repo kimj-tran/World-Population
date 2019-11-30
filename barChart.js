b8a7245be64fa15cb8b7357fd727a914b0b189bc
Julie Tran <kimtran767@gmail.com>
2019-11-29 23:45:18 -0800

    got bar going display for country

--- barChart.js
+++ barChart.js
@@ -8,3 +8,26 @@ const svg = d3.select('body')
     .attr('width', width)
     .attr('height', height)
     .style('background-color', 'lightblue')
+
+const render = data => {
+    const xScale = scaleLiner()
+        .domain(([0, max(data, d => d.population)]))
+        .range([0, width])
+
+        // max number of population
+
+    const yScale = scaleBand()
+        .domain(data.map(d => d.country))
+        .range([0, height])
+
+        // length of all country
+    svg
+      .selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("y", d => yScale(d.country))
+      .attr("width", d => xScale(d.population))
+      .attr("height", yScale.bandwidth())
+      .style("fill", "blue");
+}
