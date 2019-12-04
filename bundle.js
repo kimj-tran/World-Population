fc5d28538a5f840cfe1f4ea5a05d94d66825a194
Julie Tran <kimtran767@gmail.com>
2019-12-03 21:10:05 -0800

    added promise

--- bundle.js
+++ bundle.js
@@ -97,23 +97,37 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var d3__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! d3 */ "./node_modules/d3/index.js");
 /* harmony import */ var topojson__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! topojson */ "./node_modules/topojson/index.js");
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 
-var svg = d3__WEBPACK_IMPORTED_MODULE_0__["select"]("body").append("svg").attr("width", 1200).attr("height", 900);
-var projection = d3__WEBPACK_IMPORTED_MODULE_0__["geoOrthographic"]();
-var pathStartor = d3__WEBPACK_IMPORTED_MODULE_0__["geoPath"]().projection(projection);
-svg.append('path').attr('d', pathStartor({
+
+
+var svg = Object(d3__WEBPACK_IMPORTED_MODULE_0__["select"])("body").append("svg").attr("width", 1200).attr("height", 900);
+var projection = Object(d3__WEBPACK_IMPORTED_MODULE_0__["geoOrthographic"])();
+var pathStartor = Object(d3__WEBPACK_IMPORTED_MODULE_0__["geoPath"])().projection(projection);
+svg.append('path').attr('class', 'globe').attr('d', pathStartor({
   type: 'Sphere'
-})).attr('class', 'globe');
-Promise.all([d3__WEBPACK_IMPORTED_MODULE_0__["tsv"]("world-country-names.tsv"), d3__WEBPACK_IMPORTED_MODULE_0__["json"]("world-110m.json"), d3__WEBPACK_IMPORTED_MODULE_0__["csv"]("world.csv")]).then(function (data) {
-  return console.log(data);
-});
-d3__WEBPACK_IMPORTED_MODULE_0__["json"]("world-110m.json").then(function (data) {
-  var countries = topojson__WEBPACK_IMPORTED_MODULE_1__["feature"](data, data.objects.countries);
-  var path = svg.selectAll('path').data(countries.features).enter().append('path').attr('d', function (d) {
+}));
+Promise.all([Object(d3__WEBPACK_IMPORTED_MODULE_0__["tsv"])("world-country-names.tsv"), Object(d3__WEBPACK_IMPORTED_MODULE_0__["json"])("world-110m.json"), Object(d3__WEBPACK_IMPORTED_MODULE_0__["csv"])("world.csv")]).then(function (_ref) {
+  var _ref2 = _slicedToArray(_ref, 3),
+      tsvData = _ref2[0],
+      jsonData = _ref2[1],
+      csvData = _ref2[2];
+
+  var countryName = {};
+  tsvData.forEach(function (d) {
+    countryName[d.id] = d.name;
+  });
+  var countries = topojson__WEBPACK_IMPORTED_MODULE_1__["feature"](jsonData, jsonData.objects.countries);
+  svg.selectAll('path').data(countries.features).enter().append('path').attr('class', 'land').attr('d', function (d) {
     return pathStartor(d);
-  }).attr('class', 'land');
-  path.append('title').text('hello');
+  }).append('title').text('hello');
 });
 
 /***/ }),
--- index.js
+++ index.js
@@ -1,40 +1,44 @@
+import { select, json, tsv, geoPath, geoOrthographic, csv } from 'd3';
 import * as d3 from "d3";
 import * as topojson from 'topojson';
 
-const svg = d3.select("body")
+const svg = select("body")
   .append("svg")
   .attr("width", 1200)
   .attr("height", 900)
 
 
-const projection = d3.geoOrthographic();
-const pathStartor = d3.geoPath().projection(projection);
+const projection = geoOrthographic();
+const pathStartor = geoPath().projection(projection);
 
 svg
     .append('path')
-    .attr('d', pathStartor({type: 'Sphere'}))
     .attr('class', 'globe')
+    .attr('d', pathStartor({type: 'Sphere'}))
 
 
 Promise.all([
-  d3.tsv("world-country-names.tsv"),
-  d3.json("world-110m.json"),
-  // d3.csv("world.csv")
+  tsv("world-country-names.tsv"),
+  json("world-110m.json"),
+  csv("world.csv")
 ]).then(([tsvData, jsonData, csvData]) => {
-    // console.log(tsvData)
+
+  const countryName = {};
+  tsvData.forEach( d => {
+    countryName[d.id] = d.name
+  })
+  const countries = topojson.feature(jsonData, jsonData.objects.countries)
+
+  svg.selectAll('path')
+        .data(countries.features)
+      .enter()
+        .append('path')
+        .attr('class', 'land')
+        .attr('d', d => pathStartor(d))
+      .append('title')
+      .text('hello')
+
 });
 
 
-// d3.json("world-110m.json") 
-//     .then(data => {
-//         const countries = topojson.feature(data, data.objects.countries)
 
-//         const path = svg.selectAll('path')
-//             .data(countries.features)
-//             .enter()
-//             .append('path')
-//             .attr('class', 'country')
-//             .attr('d', pathStartor)
-//             .append('title')
-//             .text('hello')
-//     })
